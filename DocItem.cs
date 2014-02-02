73f143ce4dda5ced5fbf54bfee4bdf91adfcd6d5
Mark Dasco <Mark.Dasco@Worldnomadsgroup.com>
2014-02-03 09:04:29 +1100

    Initial Commit

---
+++ DocItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilbcsFetchFlowXmlGenerator
+{
+    public class DocItem
+    {
+        public string ItemDescription { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemQuantity { get; set; }
+    }
+}
---
+++ Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilbcsFetchFlowXmlGenerator
+{
+    public class Invoice
+    {
+        public string ClientId { get; set; }
+        public int Due { get; set; }
+        public List<DocItem> DocItems { get; set; }
+    }
+
+    public class InvoiceRow
+    {
+        public string ClientId { get; set; }
+        public int Due { get; set; }
+        public DocItem DocItem { get; set; }
+        public string ClientName { get; set; }
+        
+    }
+}
+    
+
+
