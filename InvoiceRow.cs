05c445be790c3e7d55b13278beaba68b25c19948
Mark Dasco <Mark.Dasco@Worldnomadsgroup.com>
2014-02-28 22:46:37 +1100

    Committing for March invoice run

---
+++ InvoiceRow.cs
@@ -0,0 +1,15 @@
+﻿
+using FilbcsFetchFlowXmlGenerator;
+
+namespace FilbcsFetchFlowInvoiceGenerator
+{
+    public class InvoiceRow
+    {
+        public string ClientId { get; set; }
+        public int Due { get; set; }
+        public DocItem DocItem { get; set; }
+        public string ClientName { get; set; }
+
+
+    }
+}
